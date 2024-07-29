1faafcdf4fda196f247966b5b99679d4fac894f8
Sergio Semprebom <sergiosemprebom@gmail.com>
2024-07-29 00:27:21 -0300

    exercicio for

---
+++ for.py
@@ -0,0 +1,6 @@
+
+# O comando for do python ITERA sobre os itens de qualquer sequencia seja uma lista ou uma string
+words = ['cat', 'windows', 'defenetrate']
+
+for w in words:
+    print(w, len(w))
