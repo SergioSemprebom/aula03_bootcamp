2e862538a846b809c6d8ffcb299001845206524e
Sergio Semprebom <sergiosemprebom@gmail.com>
2024-07-29 00:26:27 -0300

    exercicio if/else

---
+++ if-else.py
@@ -0,0 +1,19 @@
+# Primeira opçao:
+numero = int(input("Digite um numero: "))
+
+if numero >= 0:
+    print('O numero é par.')
+else:
+    print('O numero é impar.')
+print('============================================================================================')
+
+# segunda opçao:
+numero2 = int(input('Digite um numero: '))
+
+if numero2 % 2 == 0:
+    print('O numero é PAR!')
+
+else:
+    print('O numero é IMPAR!')
+
+print('O programa continua :) ')
